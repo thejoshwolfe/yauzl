70ca8b4c3cdb1fcd224f83c62ffb65fb132884ec
Josh Wolfe <thejoshwolfe@gmail.com>
2019-04-14 17:11:14 -0400

    testBigFile has a small file

--- test/mauCorruption.js
+++ test/mauCorruption.js
@@ -41,14 +41,190 @@ function cli() {
   });
 }
 
-function testCrowdedFile(cb) {
+function testCrowdedFile(done) {
   // TODO
-  cb();
+  done();
 }
 
-function testBigFile(cb) {
-  // TODO
-  cb();
+function testBigFile(done) {
+  var segments = [
+    {buffer: bufferFromArray([
+      // Local File Header (#0)
+      0x50, 0x4b, 0x03, 0x04, // Local file header signature
+      0x14, 0x00,             // Version needed to extract (minimum)
+      0x08, 0x08,             // General purpose bit flag
+      0x08, 0x00,             // Compression method
+      0x5a, 0x7c,             // File last modification time
+      0x8e, 0x4e,             // File last modification date
+      0x00, 0x00, 0x00, 0x00, // CRC-32
+      0x00, 0x00, 0x00, 0x00, // Compressed size
+      0x00, 0x00, 0x00, 0x00, // Uncompressed size
+      0x05, 0x00,             // File name length (n)
+      0x00, 0x00,             // Extra field length (m)
+
+      // File Name
+      0x61, 0x2e, 0x74, 0x78, 0x74,
+
+      // File Contents
+      0x03, 0x00,
+
+      // Optional Data Descriptor
+      0x50, 0x4b, 0x07, 0x08, // optional data descriptor signature
+      0x00, 0x00, 0x00, 0x00, // crc-32
+      0x02, 0x00, 0x00, 0x00, // compressed size
+      0x00, 0x00, 0x00, 0x00, // uncompressed size
+
+      // Local File Header (#1)
+      0x50, 0x4b, 0x03, 0x04, // Local file header signature
+      0x14, 0x00,             // Version needed to extract (minimum)
+      0x08, 0x08,             // General purpose bit flag
+      0x08, 0x00,             // Compression method
+      0x26, 0x83,             // File last modification time
+      0x8e, 0x4e,             // File last modification date
+      0x00, 0x00, 0x00, 0x00, // CRC-32
+      0x00, 0x00, 0x00, 0x00, // Compressed size
+      0x00, 0x00, 0x00, 0x00, // Uncompressed size
+      0x05, 0x00,             // File name length (n)
+      0x00, 0x00,             // Extra field length (m)
+
+      // File Name
+      0x62, 0x2e, 0x74, 0x78, 0x74,
+
+    ])},
+
+    {length: 8, slice: function(start, end) {
+      // File Contents TODO: replace with large data
+      console.log("slice", start, end);
+      var data = bufferFromArray([0xcb, 0x48, 0xcd, 0xc9, 0xc9, 0xe7, 0x02, 0x00]);
+      return data.slice(start - this.start, end - this.start);
+    }},
+
+    {buffer: bufferFromArray([
+      // Optional Data Descriptor
+      0x50, 0x4b, 0x07, 0x08, // optional data descriptor signature
+      0x20, 0x30, 0x3a, 0x36, // crc-32
+      0x08, 0x00, 0x00, 0x00, // compressed size TODO
+      0x06, 0x00, 0x00, 0x00, // uncompressed size TODO
+
+      // Local File Header (#2)
+      0x50, 0x4b, 0x03, 0x04, // Local file header signature
+      0x14, 0x00,             // Version needed to extract (minimum)
+      0x08, 0x08,             // General purpose bit flag
+      0x08, 0x00,             // Compression method
+      0x5a, 0x7c,             // File last modification time
+      0x8e, 0x4e,             // File last modification date
+      0x00, 0x00, 0x00, 0x00, // CRC-32
+      0x00, 0x00, 0x00, 0x00, // Compressed size
+      0x00, 0x00, 0x00, 0x00, // Uncompressed size
+      0x05, 0x00,             // File name length (n)
+      0x00, 0x00,             // Extra field length (m)
+
+      // File Name
+      0x63, 0x2e, 0x74, 0x78, 0x74,
+
+      // File Contents
+      0x03, 0x00,
+
+      // Optional Data Descriptor
+      0x50, 0x4b, 0x07, 0x08, // optional data descriptor signature
+      0x00, 0x00, 0x00, 0x00, // crc-32
+      0x02, 0x00, 0x00, 0x00, // compressed size
+      0x00, 0x00, 0x00, 0x00, // uncompressed size
+    ])},
+
+    {buffer: bufferFromArray([
+      // Central Directory Entry (#0)
+      0x50, 0x4b, 0x01, 0x02, // Central directory file header signature
+      0x3f, 0x03,             // Version made by
+      0x14, 0x00,             // Version needed to extract (minimum)
+      0x08, 0x08,             // General purpose bit flag
+      0x08, 0x00,             // Compression method
+      0x5a, 0x7c,             // File last modification time
+      0x8e, 0x4e,             // File last modification date
+      0x00, 0x00, 0x00, 0x00, // CRC-32
+      0x02, 0x00, 0x00, 0x00, // Compressed size
+      0x00, 0x00, 0x00, 0x00, // Uncompressed size
+      0x05, 0x00,             // File name length (n)
+      0x00, 0x00,             // Extra field length (m)
+      0x00, 0x00,             // File comment length (k)
+      0x00, 0x00,             // Disk number where file starts
+      0x00, 0x00,             // Internal file attributes
+      0x00, 0x00, 0xb4, 0x81, // External file attributes
+      0x00, 0x00, 0x00, 0x00, // Relative offset of local file header
+      // File name
+      0x61, 0x2e, 0x74, 0x78, 0x74,
+
+      // Central Directory Entry (#1)
+      0x50, 0x4b, 0x01, 0x02, // Central directory file header signature
+      0x3f, 0x03,             // Version made by
+      0x14, 0x00,             // Version needed to extract (minimum)
+      0x08, 0x08,             // General purpose bit flag
+      0x08, 0x00,             // Compression method
+      0x26, 0x83,             // File last modification time
+      0x8e, 0x4e,             // File last modification date
+      0x20, 0x30, 0x3a, 0x36, // CRC-32
+      0x08, 0x00, 0x00, 0x00, // Compressed size TODO
+      0x06, 0x00, 0x00, 0x00, // Uncompressed size TODO
+      0x05, 0x00,             // File name length (n)
+      0x00, 0x00,             // Extra field length (m)
+      0x00, 0x00,             // File comment length (k)
+      0x00, 0x00,             // Disk number where file starts
+      0x00, 0x00,             // Internal file attributes
+      0x00, 0x00, 0xb4, 0x81, // External file attributes
+      0x35, 0x00, 0x00, 0x00, // Relative offset of local file header
+      // File name
+      0x62, 0x2e, 0x74, 0x78, 0x74,
+
+      // Central Directory Entry (#2)
+      0x50, 0x4b, 0x01, 0x02, // Central directory file header signature
+      0x3f, 0x03,             // Version made by
+      0x14, 0x00,             // Version needed to extract (minimum)
+      0x08, 0x08,             // General purpose bit flag
+      0x08, 0x00,             // Compression method
+      0x5a, 0x7c,             // File last modification time
+      0x8e, 0x4e,             // File last modification date
+      0x00, 0x00, 0x00, 0x00, // CRC-32
+      0x02, 0x00, 0x00, 0x00, // Compressed size
+      0x00, 0x00, 0x00, 0x00, // Uncompressed size
+      0x05, 0x00,             // File name length (n)
+      0x00, 0x00,             // Extra field length (m)
+      0x00, 0x00,             // File comment length (k)
+      0x00, 0x00,             // Disk number where file starts
+      0x00, 0x00,             // Internal file attributes
+      0x00, 0x00, 0xb4, 0x81, // External file attributes
+      0x70, 0x00, 0x00, 0x00, // Relative offset of local file header TODO
+      // File name
+      0x63, 0x2e, 0x74, 0x78, 0x74,
+    ])},
+
+    {buffer: bufferFromArray([
+      // End of central directory record
+      0x50, 0x4b, 0x05, 0x06, // End of central directory signature
+      0x00, 0x00,             // Number of this disk
+      0x00, 0x00,             // Disk where central directory starts
+      0x03, 0x00,             // Number of central directory records on this disk
+      0x03, 0x00,             // Total number of central directory records
+      0x99, 0x00, 0x00, 0x00, // Size of central directory (bytes)
+      0xa5, 0x00, 0x00, 0x00, // Offset of start of central directory, relative to start of archive
+      0x00, 0x00,             // Comment Length
+    ])},
+  ];
+  var reader = new ReadBasedRandomAccessReader();
+  reader.read = makeSegmentedReadFunction(segments);
+
+  yauzl.fromRandomAccessReader(reader, segments[segments.length - 1].end, {lazyEntries: true}, function(err, zipfile) {
+    if (err) throw err;
+    if (zipfile.entryCount !== 3) throw new Error("asdf");
+    setImmediate(function() {zipfile.readEntry();});
+    zipfile.on("entry", function(entry) {
+      if (err) throw err;
+      console.log("got entry: " + entry.fileName);
+      zipfile.readEntry();
+    });
+    zipfile.on("end", function() {
+      done();
+    });
+  });
 }
 
 function testStartOffset(cb) {
@@ -155,6 +331,56 @@ function createDeflatedZeros(uncompressedSize, startOffset) {
   return stream;
 }
 
+function makeSegmentedReadFunction(segments) {
+  // precompute segments metadata
+  var cursor = 0;
+  for (var i = 0; i < segments.length; i++) {
+    if (segments[i].start == null) {
+      segments[i].start = cursor;
+    } else if (segments[i].start !== cursor) throw new Error("bad segment start: " + i);
+    if (segments[i].buffer != null) {
+      if (segments[i].end == null) {
+        segments[i].end = cursor + segments[i].buffer.length;
+      } else if (segments[i].end !== cursor + segments[i].buffer.length) throw new Error("bad segment length: " + i);
+    } else {
+      if (segments[i].end == null) {
+        segments[i].end = cursor + segments[i].length;
+      } else if (segments[i].end !== cursor + segments[i].length) throw new Error("bad segment length: " + i);
+    }
+    cursor = segments[i].end;
+  }
+
+  return function read(buffer, offset, length, position, callback) {
+    console.log("read(0x" + position.toString(16) + "..0x" + (position + length).toString(16) + ")");
+    // why even have parameters for `offset` and `length`?
+    buffer = buffer.slice(offset, offset + length);
+    for (var i = 0; i < segments.length; i++) {
+      var segment = segments[i];
+      if (position + length <= segment.start || segment.end <= position) continue;
+      //console.log("segment:", i); // TODO: tmp debugging
+      // this segment contributes something
+      if (segment.buffer != null) {
+        // direct copy
+        var bytesCopied = segment.buffer.copy(buffer,
+          Math.max(0, segment.start - position),
+          Math.max(0, position - segment.start));
+        buffer = buffer.slice(bytesCopied);
+        position += bytesCopied;
+
+        if (buffer.length === 0) return callback();
+      } else {
+        var buf = segment.slice(position, position + Math.min(buffer.length, segment.length));
+        var bytesCopied = buf.copy(buffer)
+        buffer = buffer.slice(bytesCopied);
+        position += bytesCopied;
+
+        if (buffer.length === 0) return callback();
+      }
+    }
+    throw new Error("nothing left to read");
+  };
+}
+
 // TODO: this class could probably be exported by yauzl proper.
 util.inherits(ReadBasedRandomAccessReader, yauzl.RandomAccessReader);
 function ReadBasedRandomAccessReader() {
@@ -165,11 +391,13 @@ ReadBasedRandomAccessReader.prototype._readStreamForRange = function(start, end)
   var stream = new Readable();
   var bytesRead = 0;
   stream._read = function(size) {
+    size = 3; // TODO: tmp
     var selfStream = this;
     pump();
     function pump() {
       var readStart = start + bytesRead;
       var readSize = Math.min(size, end - readStart);
+      console.log("readSize:", readSize); // TODO: debugging
       if (readSize <= 0) {
         return selfStream.push(null);
       }
